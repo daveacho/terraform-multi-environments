042566317537e5acc4bfbe0fb49892433cd80dab
david achoja <david.achoja@dumarey.com>
2025-07-22 14:10:01 +0100

    updated remote s3 backend

--- terraform/prod/remote_backend.tf
+++ terraform/prod/remote_backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket       = "my-first-practice-bucket-aaa"
-    key          = "statefiles/terraform.tfstate"
+    key          = "statefiles-prod/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = false
   }
